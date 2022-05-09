d93f6acdbd2e2df73a0cc5a612fdd3837d4fcd82
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-05-09 21:51:09 +0530

    Time: 2 ms (59.98%), Space: 8.3 MB (70.71%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
+        int n =nums.size();
         
-        int l = 0;
-        int m = 0;
-        int h = n - 1;
+        int l = 0;  	// low pointer
+        int m = 0;      // mid pointer
+        int h = n -1;      // high ponter
         
         while(m <= h){
-            switch(nums[m]){
-                    
+            switch(nums[m]) {
                 case 0:
                     swap(nums[l++], nums[m++]);
                     break;
                     
                 case 1:
-                    m++;
+    	            m++;
                     break;
                     
                 case 2:
-                    swap(nums[m],nums[h--]);
+                    swap(nums[m], nums[h--]);                    
                     break;
+                
             }
         }
     }

ba009e76fffe02dc9282f71ded21a71ae59d04a1
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-05-13 20:29:08 +0530

    Time: 7 ms (14.86%), Space: 6.4 MB (26.14%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    map <int, int> terminate;
+    bool isHappy(int n) {
+        
+        int temp = 0;
+        if(n ==1){
+            return true;
+        }
+        
+        while(n != 0){
+            temp += pow(n%10, 2);
+            n = n/10;
+        }
+        if(terminate[temp] == 0){
+            terminate[temp] = 1;
+        } else{
+            return 0;
+        }
+        return isHappy(temp);
+    }
+};

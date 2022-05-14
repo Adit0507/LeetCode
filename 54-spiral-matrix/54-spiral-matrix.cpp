ad77de171bcee4fda460a910c20a4bd6a3ded9db
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-05-14 13:26:12 +0530

    Time: 3 ms (36.64%), Space: 6.9 MB (32.01%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -1,62 +1,58 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> result;
-
+        vector <int> result;
+        
         int row = matrix.size();
-
-        if( row == 0 ) {
+        
+        if(row == 0){
             return result;
         }
-
+        
         int col = matrix[0].size();
-
-        if( row == 0 && col == 0 ) {
+        
+        if(row == 0 && col == 0){
             return result;
         }
-
-        if( row == 1 && col == 1 ) {
+        
+        if(row == 1 && col == 1){
             result.push_back(matrix[0][0]);
             return result;
         }
-
-        int left = 0, right = col;
+        
+        int l = 0, r = col;
         int top = 0, bottom = row;
-
-
-        while(top < bottom && left < right) {
-
-            if( top < bottom ) {
-                for( int i = left; i < right ; i++ ) {
+        
+        
+        while(top < bottom && left < right){
+            if(top < bottom){
+                for(int i = l; i < r;i++){
                     result.push_back(matrix[top][i]);
                 }
-                top++;
+                top ++;
             }
-
-            if( left < right ) {
-                for( int i = top; i < bottom; i++) {
-                    result.push_back(matrix[i][right-1]);
+            
+            if(l < r){
+                for(int i = top; i < bottom; i++){
+                    result.push_back(matrix[i][r - 1]);
                 }
-                right--;
+                r --;
             }
-
+            
             if( top < bottom ) {
-                for( int i = right - 1; i >= left ; i-- ) {
+                for( int i = r - 1; i >= l ; i-- ) {
                     result.push_back(matrix[bottom-1][i]);
                 }
                 bottom--;
             }
-
-            if( left < right ) {
-                for( int i = bottom - 1 ; i >= top ; i-- ) {
-                    result.push_back(matrix[i][left]);
-                }
-                left++;
-            }
-
-        }
-
-        return result;
             
+            if(l < r){
+                for( int i = bottom - 1 ; i >= top ; i-- ) {
+                    result.push_back(matrix[i][l]);
+                }
+                l++;
+            }
+        }
+        return result;        
     }
 };

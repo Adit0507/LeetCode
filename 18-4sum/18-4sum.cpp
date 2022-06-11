9d7b10c3e499a364442eb45a63f060df0b04b95f
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-06-11 12:35:08 +0530

    Time: 660 ms (10.67%), Space: 78.7 MB (6.84%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        set<vector<int>>ans1;
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n-3;i++)
-        {
-            for(int j=i+1;j<n-2;j++)
-            {
-                int left=j+1;
-                int right=n-1;
-                while(left<right)
-                {
-                    int sum1=nums[i]+nums[j];
-                    int sum2=target-(nums[left]+nums[right]);
-                    if(sum2<sum1)
-                    {
-                        right--;
+        set <vector<int>> ans1;
+        int n = nums.size();
+        
+        sort(nums.begin(), nums.end());
+        
+        for(int i = 0; i < n-3; i++){
+            for(int j = i+1; j < n -2; j++){
+                int l = j + 1;
+                int r = n - 1;
+                
+                while(l < r){
+                    int s1 = nums[i] + nums[j];
+                    int s2 = target - (nums[l] + nums[r]);
+                    
+                    if(s2 < s1){
+                        r --;
                     }
-                    else if(sum1==sum2)
+                    
+                    else if(s1 == s2)
                     {
-                        vector<int>v{nums[i],nums[j],nums[left],nums[right]};
+                        vector<int>v{nums[i],nums[j],nums[l],nums[r]};
                         ans1.insert(v);
-                        left++;
+                        l++;
                     }
-                    else
-                    {
-                        left++;
+                    else {
+                        l ++;
                     }
                 }
             }
         }
-        vector<vector<int>>ans(ans1.begin(),ans1.end());
+        vector<vector<int>> ans(ans1.begin(), ans1.end());
         return ans;
-        
     }
 };

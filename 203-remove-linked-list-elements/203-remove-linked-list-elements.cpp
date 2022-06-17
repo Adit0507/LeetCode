48aa1682823d1bda02b031f59da09994fb734086
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-06-17 18:18:35 +0530

    Time: 29 ms (68.26%), Space: 16 MB (10.22%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
@@ -11,9 +11,7 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        if(head == NULL)
-            return NULL;
-        
+        if(head == NULL)return NULL;
        head->next = removeElements(head->next, val);
        
         if(head->val == val){

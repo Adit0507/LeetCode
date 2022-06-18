d0a141732f509d3d05deef4a14a52c5eb73db086
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-06-18 23:26:57 +0530

    Time: 115 ms (5.01%), Space: 24.2 MB (19.49%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindromeRange(string s, int i, int j){
+        while (i < j) {
+            if(s[i++] != s[j--])
+                return false;
+        }
+        return true;
+    }
+    
+    bool validPalindrome(string s) {
+        for(int i = 0; i < s.size()/2; i++){
+            int j = s.size()-1-i;
+            if(s[i] != s[j]){
+                return isPalindromeRange(s, i+1, j) || 
+                    isPalindromeRange(s, i, j-1);
+            }
+        }
+        return true;
+    }
+};

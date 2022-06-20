dc1eec638b252b7898e6160e282cd792251844df
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-06-20 15:12:33 +0530

    Time: 29 ms (53.05%), Space: 18.4 MB (83.76%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
@@ -7,10 +7,11 @@ public:
             return 0;
         
         int ans = 0, l= 0, r = 0;
+        // l = increasing and r = decreasing in this case
         
         for(int i = 1; i < n; i++){
             
-            if(arr[i] > arr[i - 1])
+            if(arr[i] > arr[i - 1]) // Increasing count
             {
                 if(r) {
                     l = 0; 
@@ -18,13 +19,13 @@ public:
                 }
                 l += (1 + (l==0));
             }
-            else if (arr[i] < arr[i - 1] && l)
+            else if (arr[i] < arr[i - 1] && l)  // r = decreasing count
                 r += 1;
-            else {
+            else {      // same numbers
                 l = 0;
                 r = 0;
             }
-            ans = max((l + r) * (l && r), ans);
+            ans = max((l + r) * (l && r), ans);   // maximize the ans
         }
         return ans;
     }

c8f05394836152e79c716939b0f970db1df4cbf8
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-07-10 11:56:33 +0530

    Time: 24 ms (82.49%), Space: 9.5 MB (76.37%) - LeetHub

--- 147-insertion-sort-list/147-insertion-sort-list.cpp
+++ 147-insertion-sort-list/147-insertion-sort-list.cpp
@@ -11,19 +11,40 @@
 class Solution {
 public:
     ListNode* insertionSortList(ListNode* head) {
-         ListNode dummy(0);
-    ListNode* prev = &dummy;  // the last (largest) of the sorted list
-
-    while (head) {                  // current inserting node
-      ListNode* next = head->next;  // cache next inserting node
-      if (prev->val >= head->val)   // `prev` >= current inserting node
-        prev = &dummy;              // move `prev` to the front
-      while (prev->next && prev->next->val < head->val)
-        prev = prev->next;
-      head->next = prev->next;
-      prev->next = head;
-      head = next;  // update current inserting node
-    }
-        return dummy.next;
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode *curr =head, *prev= dummy;
+        
+        while(curr){
+            if(curr->next && (curr->next->val < curr->val)){
+                while(prev->next && (prev->next->val < curr->next->val))
+                    prev = prev->next;
+                ListNode* temp = prev->next;
+                prev->next = curr->next;
+                curr->next = curr->next->next;
+                prev->next->next = temp;
+                prev = dummy;
+            } else{
+                curr = curr->next;
+            }
+        }
+        return dummy->next;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

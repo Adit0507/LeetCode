a72b3b4c480a2ce791cb6e3ce3aff43b6e5c2b51
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-07-11 10:08:57 +0530

    Time: 3 ms (84.17%), Space: 11.9 MB (88.71%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -11,19 +11,27 @@
  */
 class Solution {
 public:
-    void rightView(TreeNode* root, int level, vector<int> &ans){
+    void right(TreeNode* root, int level, vector<int> &ans){
         if(root == NULL) return;
         if(ans.size() == level) 
             ans.push_back(root->val);
-        rightView(root->right, level + 1, ans);
-        rightView(root->left, level + 1, ans);  
+        right(root->right, level+1, ans);
+        right(root->left, level+1, ans);
     }
     vector<int> rightSideView(TreeNode* root) {
         vector<int> res;
-        rightView(root, 0, res);
+        right(root, 0, res);
         return res;
     }
 };
+
+
+
+
+
+
+
+
 
 
 

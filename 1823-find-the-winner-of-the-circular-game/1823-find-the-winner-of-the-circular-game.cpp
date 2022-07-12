9f537eff43337c98a3941a8ef6a340e8d56d07be
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-07-12 21:19:13 +0530

    Time: 0 ms (100.00%), Space: 6 MB (73.85%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        int a= 0;
+        for(int i=1; i <=n; i++){
+            a = (a + k)%i;
+        }
+        return a +1;
+    }
+};

b9279672a272b52bb92949862fae1f0d1754ab6d
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-07-13 11:49:37 +0530

    Time: 195 ms (94.03%), Space: 81.9 MB (59.33%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> A(n);
+        deque<int> q;
+        q.push_back(n-1);
+        A[n-1] = nums[n -1];
+        for(int i = n-2; i >= 0; i--){
+            if(q.front() -i > k)
+                q.pop_front();
+            A[i] = nums[i] + A[q.front()];
+            while(q.size() && A[q.back()] < A[i])
+                q.pop_back();
+            q.push_back(i);
+        } 
+        return A[0];
+    }
+};

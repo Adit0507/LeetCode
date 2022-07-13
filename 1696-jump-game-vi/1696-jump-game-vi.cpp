5af7005c5ee7b919127f4c1ec50806eca3a57a00
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-07-13 11:50:13 +0530

    Time: 260 ms (78.12%), Space: 81.8 MB (62.94%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -6,9 +6,11 @@ public:
         deque<int> q;
         q.push_back(n-1);
         A[n-1] = nums[n -1];
+        
         for(int i = n-2; i >= 0; i--){
             if(q.front() -i > k)
                 q.pop_front();
+           
             A[i] = nums[i] + A[q.front()];
             while(q.size() && A[q.back()] < A[i])
                 q.pop_back();

b1f7d92143eb262a30fcfa5bf97ccf8a69882aa6
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-07-22 12:50:14 +0530

    Time: 7 ms (73.71%), Space: 10.3 MB (32.53%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -11,31 +11,33 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-                
-        ListNode* list1 = new ListNode (0);
-        ListNode* list2 = new ListNode (0);
+        ListNode* list1 = new ListNode(0);
+        ListNode* list2 = new ListNode(0);
         
-        ListNode* l1 = list1;
-        ListNode* l2 = list2;
-        
-        while (head){
-            if(head -> val < x){
+        // For iterating over the elements
+        ListNode*l1 = list1, *l2 = list2;
+        while(head){
+            if(head->val < x){
                 l1 ->next = head;
                 l1 = l1->next;
+            } else{
+                l2 ->next = head;
+                l2= l2->next;
             }
-            
-            else{
-                l2->next = head;
-                l2 = l2->next;
-            }
-            head = head->next;
+            // Will keep iterating over all the elements
+            head = head ->next;
         }
-        l1 ->next = list2->next;
+        l1->next = list2->next;
         l2 ->next = NULL;
         
         return list1->next;
     }
 };
+
+
+
+
+
 
 
 

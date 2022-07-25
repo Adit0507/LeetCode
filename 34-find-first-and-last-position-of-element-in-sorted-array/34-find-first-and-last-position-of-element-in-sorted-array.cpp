f1bb73598baf6071dd2ba4621049426294103d62
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-07-25 10:43:24 +0530

    Time: 20 ms (14.10%), Space: 13.7 MB (17.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,48 +1,53 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();   
-            vector <int> res;
-          
-            if(nums.empty()){
-                return vector <int> {-1 , -1};
-            } 
-        
-            int l = 0, r = nums.size() - 1;
-                while(l +1 < r){
-                    int mid = l + (r-l)/ 2;
-                    
-                    if(nums[mid] < target){
-                        l= mid;
-                    }else {
-                        r = mid;
-                    }
-                }
-                        
-            if(nums[l] == target){
-                res.push_back(l);
-            } else if(nums[r] == target){
-                res.push_back(r);
-            }else {
-                return vector<int> {-1,-1};
+            int n = nums.size();
+            vector<int> ans;
+            if(nums.empty())
+                return vector<int> {-1, -1};
+
+            int l =0, r = nums.size() -1;
+            while(l +1 < r){
+                int mid = l + (r - l)/2;
+                if(nums[mid] < target)
+                    l = mid;
+                else
+                    r = mid;
             }
         
-        l = 0, r = nums.size() - 1;
-        while(l + 1 < r){
+        if(nums[l] == target)
+            ans.push_back(l);
+        else if(nums[r] == target)
+            ans.push_back(r);
+        else
+            return vector<int> {-1, -1};
+        
+        l =0, r=nums.size()-1;
+        while(l +1 < r){
             int mid = l + (r-l)/2;
-            if(nums[mid] <= target){
+            if(nums[mid] <= target)
                 l = mid;
-            } else{
+            else
                 r = mid;
-            }
         }
+        if(nums[r] == target)
+            ans.push_back(r);
+        else if(nums[l] == target)
+            ans.push_back(l);
         
-        if(nums[r] == target){
-            res.push_back(r);
-        } else if(nums[l] == target){
-            res.push_back(l);
-        }
- 
-        return res;
+        return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+

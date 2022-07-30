c6537510dd70ae6f14da940db85009712f9ff585
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-07-30 22:15:21 +0530

    Time: 86 ms (87.09%), Space: 28 MB (49.38%) - LeetHub

--- 436-find-right-interval/436-find-right-interval.cpp
+++ 436-find-right-interval/436-find-right-interval.cpp
@@ -6,6 +6,7 @@ public:
         for(int i=0; i < intervals.size(); i++)
             m[intervals[i][0]] = i;
         for(int i = 0; i < intervals.size(); i++)
+            // lower_bound is used to return an iterator pointing to the first element in range [first, last] whihc has a value not less than val
             ans[i] = m.lower_bound(intervals[i][1]) != end(m) ? m.lower_bound(intervals[i][1])->second : -1;
         return ans;
     }

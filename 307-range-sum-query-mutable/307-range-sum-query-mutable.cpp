5771a54d3cf0496b45a79154b85fab15670c70d6
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-07-31 18:13:28 +0530

    Time: 806 ms (25.86%), Space: 149.9 MB (94.89%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -1,64 +1,30 @@
 class NumArray {
-    int n;
-    vector <int> seg;
-    
-    int build(const vector<int> &nums, int l, int r, int node){
-        int mid;
-        int leftSum, rightSum;
-        
-        if(l ==r)
-            return seg[node] = nums[l];
-        mid = (l + r)/2;
-        leftSum  =build(nums, l, mid, 2*node +1);
-        rightSum = build(nums, mid +1, r, 2*node + 2);
-        
-        return seg[node] = leftSum + rightSum;
-    }
-    
-    int sumRange(int l, int r, int ss, int se, int node){
-        int mid;
-        int leftSum, rightSum;
-        
-        if(r < ss || l > se)
-            return 0;
-        if(l <= ss && se <= r)
-            return seg[node];
-        
-        mid = (ss + se)/2;
-        leftSum = sumRange(l, r, ss, mid, 2*node + 1);
-        rightSum = sumRange(l, r, mid +1, se, 2*node +2);
-        
-        return leftSum + rightSum;
-    }
-    
-    int update(int index, int newVal, int ss, int se, int node){
-        int leftSum, rightSum;
-        int mid;
-        
-        if(index < ss || index > se)
-            return seg[node];
-        if(ss == se)
-            return seg[node] = newVal;
-        mid = (ss + se)/2;
-        leftSum = update(index, newVal, ss, mid, 2*node + 1);
-        rightSum = update(index, newVal, mid+ 1, se, 2*node + 2);
-        
-        return seg[node] = leftSum + rightSum;
-    }
-    
 public:
+    vector<int> v;
+    int sum = 0;
     NumArray(vector<int>& nums) {
-        n =nums.size();
-        seg.resize(4*n);
-        build(nums, 0, n -1, 0);
+        v = nums;
+        sum = 0;
+        for(int i=0; i < nums.size(); i++){
+            sum += nums[i];
+        }
     }
     
     void update(int index, int val) {
-        update(index, val, 0, n-1, 0);
+        sum -= v[index];
+        v[index] = val;
+        sum += val;
     }
     
     int sumRange(int left, int right) {
-        return sumRange(left, right, 0, n -1, 0);
+        int res = sum;
+        for(int i=0; i < left; i++){
+            res -= v[i];
+        }
+        for(int i= right + 1; i < v.size(); i++){
+            res -= v[i];
+        }
+        return res;
     }
 };
 

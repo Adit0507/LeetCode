99ab350ae1f6cb8ecf9476f21de0e5dc9d04b2e4
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-08-01 10:11:12 +0530

    Time: 0 ms (100.00%), Space: 6 MB (75.04%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int N = n + m -2;
-        int r = m -1;
-        double res = 1;
-        
-        for(int i = 1; i <= r; i++)
-            res = res*(N- r + i)/i;
-        return (int)res;
+        int grid[m][n];
+        for(int i= m-1; i >= 0; i--)
+            for(int j= n -1; j >=0; j--){
+                if(i == m-1 || j== n -1)
+                    grid[i][j] = 1;
+                else
+                    grid[i][j] = grid[i + 1][j] + grid[i][j +1];
+            }
+        return grid[0][0];
     }
 };

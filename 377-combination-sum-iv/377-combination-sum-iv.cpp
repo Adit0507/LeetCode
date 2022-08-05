bdde37e8c90005447034deb6d9ebb3c4140d2a64
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-08-05 19:11:56 +0530

    Time: 4 ms (46.34%), Space: 6.8 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target +1, -1);
-        return solve(nums, nums.size(), target, dp);
+        return solve(nums, target, nums.size());
     }
-    int solve(vector<int> &arr, int len, int target, vector<int> &dp){
-        if(target < 0)
-            return 0;
-        if(target == 0)
-            return 1;
-        
-        if(dp[target] != -1)
-            return dp[target];
-        
-        int ans = 0;
-        for(int i=0; i < len; i++){
-            ans += solve(arr, len, target -arr[i], dp);
+    
+    unsigned int solve(vector<int>arr, int target, int l){
+        vector<unsigned int> dp(target +1, 0);
+        // for target 0 ans is 1
+        dp[0] =1;
+        // traversing ebery target
+        for(int i=1; i <= target; i++){
+            // traversing array
+            for(int j =0; j< l; j++){
+                //check if the target is going -ve
+                if(i - arr[j] >= 0)
+                    dp[i] += dp[i - arr[j]];
+            }
         }
-        return dp[target] = ans;
+        return dp[target];        
     }
 };

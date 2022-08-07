0e1b46472bfa18c24b43f10d91f2705b78bf720b
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-08-07 11:02:00 +0530

    Time: 5 ms (88.63%), Space: 6 MB (85.05%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countVowelPermutation(int n) {
+        long long a=1, b=1, c=1, d=1,e =1;
+        long sum = 0;
+        int mod=1000000007;
+        
+        for(int i = 0; i < n; i++){
+            sum = (a +b+c+d+e)%mod;
+            
+            long long arr[5] = {a,b,c,d,e};
+            a = (arr[1] + arr[2] + arr[4])%mod;
+            b = (arr[0] + arr[2]) %mod;
+            c = (arr[1] + arr[3]) %mod;
+            d = (arr[2])%mod;
+            e = (arr[2] + arr[3]) %mod;
+        }
+        return sum%mod;
+    }
+};

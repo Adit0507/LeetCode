e02d3b44e2cfa0a5cc335d557b1f087acde9e3ef
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-08-20 11:55:51 +0530

    Time: 45 ms (58.63%), Space: 16.5 MB (17.69%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -7,9 +7,9 @@ public:
         int i = 0;
         priority_queue <long> pq;
         while(i < stations.size()){
-            if(range >= target)
+            if(range >= target)        // Ability to reach the target
                 return ans;
-            if(range < stations[i][0]){
+            if(range < stations[i][0]){     // We refuel, but stay on the same index of the pump, we need to make sure that, we reacjhed till its position
                 if(pq.empty())
                     return -1;
                 range += pq.top();
@@ -17,11 +17,11 @@ public:
                 ans ++;
                 continue;
             }
-            else
+            else                // We can pass this pump, no refuel needed 
                 pq.push(stations[i][1]);
             i++;
         }
-        while(range < target && !pq.empty()){
+        while(range < target && !pq.empty()){   // Check if we can reach the target or not
             range += pq.top();
             pq.pop();
             ans ++;

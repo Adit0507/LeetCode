8dd3a5db3cb4c766bd31fc563f3dbc84dc94d024
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-10-04 11:41:47 +0530

    Time: 247 ms (59.64%), Space: 95.5 MB (50.13%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        int totalTime = 0;
-        int i =0, j = 0;
-        
-        while(i < neededTime.size() && j < neededTime.size()){
-            int curTotal = 0, curMax = 0;
-            
-            while(j < neededTime.size() && colors[i] == colors[j]){
-                curTotal += neededTime[j];
-                curMax = max(curMax, neededTime[j]);
-                j++;
-            }
-            totalTime += curTotal - curMax;
-            i = j;
+       // totalTime: Time needed to make rope colorful
+      // currMaxTime: max. time of a balloon needed in the group
+        int totalTime = 0, currMax = 0;
+
+        //For each balloon in the array
+        for(int i = 0;i < colors.size(); i++){
+            // If this balllon is 1st ballon of a new group set the currMax as 0
+            if(i > 0 && colors[i] != colors[i -1])
+                currMax = 0;
+//          Inc. totalTime by smaller one
+            // update currMax as the larger one
+            totalTime += min(currMax, neededTime[i]);
+            currMax = max(currMax, neededTime[i]);
         }
         return totalTime;
     }

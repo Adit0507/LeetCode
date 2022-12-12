add30c4c76b5e7711cebc67d1a44265604ed7d01
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-12-12 23:10:42 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.04%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> ans(n+1);
+        
+        ans[0]= 1;
+        ans[1]= 1;
+        
+        for(int i=2; i< n +1; i++){
+            ans[i]= ans[i-1] + ans[i-2];
+        }
+        return ans[n];
+    }
+};

8e3b08f59c7449e468859654883942b0f47aff10
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2022-12-15 13:08:38 +0530

    Time: 150 ms (66.61%), Space: 6.7 MB (95.09%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -7,10 +7,8 @@ public:
         
         for(int i= 1; i <= n; i++){
             for(int j=1; j <= m; j++){
-                if(s[i -1] == t[j-1])
-                    curr[j]= 1 + prev[j -1];
-                else
-                    curr[j] = max(prev[j], curr[j-1]);
+                if(s[i -1] == t[j-1])curr[j]= 1 + prev[j -1];
+                else curr[j] = max(prev[j], curr[j-1]);
             }
             prev= curr;
         }
@@ -20,7 +18,6 @@ public:
     int longestPalindromeSubseq(string s) {
         string t= s;
         reverse(s.begin(), s.end());
-        
         return lcs(s, t);
     }
 };

98c19c65131daebacc5a42da5f5acd0983633d6c
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2023-01-02 14:32:24 +0530

    Time: 35 ms (64.23%), Space: 16.8 MB (76.42%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.cpp
+++ 0641-design-circular-deque/0641-design-circular-deque.cpp
@@ -1,85 +1,63 @@
 class MyCircularDeque {
 private:
-    vector<int> buffer;
-    int cnt;
-    int k;
-    int front;
-    int rear;
+    vector<int>buffer;
+    int cap, k, front, rear;
 public:
-    /** Initialize your data structure here. Set the size of the deque to be k. */
-    MyCircularDeque(int k): buffer(k, 0), cnt(0), k(k), front(k - 1), rear(0) {
+    MyCircularDeque(int k): buffer(k, 0), cap(0), k(k), front(k-1), rear(0) {
     }
     
-    /** Adds an item at the front of Deque. Return true if the operation is successful. */
     bool insertFront(int value) {
-        if (cnt == k) {
+        if(cap== k)
             return false;
-        }
-        buffer[front] = value;
-        front = (front - 1 + k) % k;
-        ++cnt;
-
+        buffer[front]= value;
+        front= (front -1+ k)%k;
+        ++cap;
         return true;
     }
     
-    /** Adds an item at the rear of Deque. Return true if the operation is successful. */
     bool insertLast(int value) {
-        if (cnt == k) {
+        if(cap==k)
             return false;
-        }
-        buffer[rear] = value;
-        rear = (rear + 1) % k;
-        ++cnt;
-
+        buffer[rear]= value;
+        rear= (rear +1)%k;        
+        ++cap;
         return true;
     }
     
-    /** Deletes an item from the front of Deque. Return true if the operation is successful. */
     bool deleteFront() {
-        if (cnt == 0) {
+        if(cap== 0)        
             return false;
-        }
-        front = (front + 1) % k;
-        --cnt;
-
+        front = (front +1)%k;
+        --cap;
         return true;
     }
     
-    /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
     bool deleteLast() {
-        if (cnt == 0) {
+        if(cap== 0)
             return false;
-        }
-        rear = (rear - 1 + k) % k;
-        --cnt;
-
+        rear= (rear - 1+k)%k;
+        --cap;
         return true;
     }
     
-    /** Get the front item from the deque. */
     int getFront() {
-        if (cnt == 0) {
+        if(cap== 0)
             return -1;
-        }
-        return buffer[(front + 1) % k];
+        return buffer[(front +1)%k];
     }
     
-    /** Get the last item from the deque. */
     int getRear() {
-        if (cnt == 0) {
+         if (cap == 0) 
             return -1;
-        }
         return buffer[(rear - 1 + k) % k];
     }
     
-    /** Checks whether the circular deque is empty or not. */
     bool isEmpty() {
-        return cnt == 0;
+        return cap== 0;
     }
     
-    /** Checks whether the circular deque is full or not. */
     bool isFull() {
-        return cnt == k;
+        return cap== k;
     }
 };
 

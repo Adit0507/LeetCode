f25ab84287aea8378da49eb7b56dbf719255d36c
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2023-01-11 07:22:31 +0530

    Time: 303 ms (55.03%), Space: 81.1 MB (14.76%) - LeetHub

---
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    unordered_map<int, vector<int>>g;
+    unordered_map<int, bool> vis;   // visited nodes
+    
+    void createGraph(vector<vector<int>>&edges){
+        for(auto e:edges){
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]);
+        }
+    }
+    int dfs(int node, int myCost, vector<bool>&hasApple){
+        if(vis[node])
+            return 0;
+        vis[node] = true;
+        
+        int childCost= 0;
+        for(auto x: g[node])
+            childCost += dfs(x, 2,hasApple);
+            
+        if(childCost== 0 && hasApple[node]== false)
+            return 0;
+        return (childCost +myCost);
+    }
+    
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        createGraph(edges);
+        return dfs(0, 0, hasApple);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

34b648a7f564ca2747865ff054ca2d232c708e65
Aditya Kumar Singh <78037846+Adit0507@users.noreply.github.com>
2023-01-11 07:44:30 +0530

    Time: 666 ms (55.98%), Space: 185.1 MB (31.78%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, vector<int>> v;
+    
+    void createGraph(TreeNode* root){
+        queue<pair<TreeNode*, int>>q;
+        q.push({root, -1});
+        
+        while(q.size()){
+            auto [node, parent] = q.front();
+            q.pop();
+            if(parent !=-1){
+                v[parent].push_back(node->val);
+                v[node->val].push_back(parent);
+            }
+            if(node->left)
+                q.push({node->left, node->val});
+            if(node->right)
+                q.push({node->right, node->val});
+        }
+    }
+    
+    int amountOfTime(TreeNode* root, int start) {
+        createGraph(root);
+            
+        queue<int> q;
+        unordered_map<int, bool>seen;
+        q.push(start);
+        seen[start]= true;
+        int time= 0;
+        for(; q.size(); time++){
+            int n= q.size();
+            while(n--){
+                auto node= q.front();
+                q.pop();
+                for(auto i:v[node]){
+                    if(!seen[i])
+                        q.push(i);
+                        seen[i]= true;
+                }
+            }
+        }
+        return time-1;
+    }
+};
